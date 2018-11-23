64d6170a5068ac5d1c64e3323311785ab26ed589
ccrahul <45008654+ccrahul@users.noreply.github.com>
2018-11-23 17:07:36 +0530

    Update move-step1.ps1 (#1)
    Clone from master and added a comment

--- move-step1.ps1
+++ move-step1.ps1
@@ -1,4 +1,6 @@
+Write-Host "This is a branch from Master"
 Write-Host "Added to Phase 1"
+Write-Host "Added to Phase 2"
 
 #$logfile = "C:\Users\933621\Desktop\logs\log_$(get-date -format `"yyyyMMdd_hhmmsstt`").txt"
 #function log($string)
